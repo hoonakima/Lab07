e0a6d6131f394be6d4f42d558eb349a6dcaf4498
JoJeonghoon <akima95@naver.com>
2018-04-03 20:50:18 +0900

    Add a main method

--- src/main/java/edu/handong/csee/java/lab07/prob1/YearToCentury_main.java
+++ src/main/java/edu/handong/csee/java/lab07/prob1/YearToCentury_main.java
@@ -1,10 +1,22 @@
 package edu.handong.csee.java.lab07.prob1;
 
+import java.util.Scanner;
+
 public class YearToCentury_main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+		Scanner keyboard = new Scanner(System.in);
+		
+		System.out.println("Input year: ");
+		int year = keyboard.nextInt();
+		
+		YearToCentury obj = new YearToCentury(year);
+		
+		System.out.println(year + " is " + obj.calCentury() + "th century." );
+	
+		
 	}
 
 }
