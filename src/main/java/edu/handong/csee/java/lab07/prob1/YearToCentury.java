b2f9f0ad2392331bde774f0fbd03c0f8e687460e
JoJeonghoon <akima95@naver.com>
2018-04-03 21:38:35 +0900

    Add single line comments

--- src/main/java/edu/handong/csee/java/lab07/prob1/YearToCentury.java
+++ src/main/java/edu/handong/csee/java/lab07/prob1/YearToCentury.java
@@ -1,4 +1,4 @@
-package edu.handong.csee.java.lab07.prob1; 
+package edu.handong.csee.java.lab07.prob1;  //package name
 
 /**
  * defines YearToCentury object.
@@ -6,27 +6,27 @@ package edu.handong.csee.java.lab07.prob1;
  * @author jo jeong hoon
  *
  */
-public class YearToCentury { 
+public class YearToCentury { //defines YearToCentury class
 
-	int year;
-	
-	public YearToCentury() {
-		year =0;
+	int year; //instance variable
+
+	public YearToCentury() { //default constructor
+		year =0; //default year value when default constructor is called
 	}
-	
-	public YearToCentury(int year) {
-		this.year =year;
+
+	public YearToCentury(int year) { //constructor which has int parameter
+		this.year =year; //puts the parameter value into the instance variable 
 	}
-	
+
 	/**
 	 * returns century number from given year value
 	 * @return refers century number
 	 */
-	public int calCentury() {
-		
-		if(year%100 ==0)
-			return  year/100;
-		return (year/100)+1;
+	public int calCentury() { //defines calCentury method
+
+		if(year%100 ==0) //when the year value is ended with 00
+			return  year/100; //in this case, the century number is year/100 
+		return (year/100)+1; // when year value is ended with 00, the century number is year/100 +1
 	}
-	
+
 }
--- src/main/java/edu/handong/csee/java/lab07/prob1/YearToCentury_main.java
+++ src/main/java/edu/handong/csee/java/lab07/prob1/YearToCentury_main.java
@@ -1,31 +1,30 @@
-package edu.handong.csee.java.lab07.prob1;
+package edu.handong.csee.java.lab07.prob1; //package name
 
-import java.util.Scanner;
+import java.util.Scanner; //import Scanner class to get input
 
 /**
  * This class is for executing the program that uses YearToCentury class.
  * @author jo jeong hoon
  *
  */
-public class YearToCentury_main {
+public class YearToCentury_main { //defines YearToCentury_main class
 
 	/**
 	 * starts this program.
 	 * @param args refers command-line arguments as an array of String objects.
 	 */
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	public static void main(String[] args) { //define a main method
 
-		Scanner keyboard = new Scanner(System.in);
-		
-		System.out.println("Input year: ");
-		int year = keyboard.nextInt();
-		
-		YearToCentury obj = new YearToCentury(year);
-		
-		System.out.println(year + " is " + obj.calCentury() + "th century." );
-	
-		
+		Scanner keyboard = new Scanner(System.in); //Instantiation the Scanner object
+
+		System.out.println("Input year: "); //print the line 
+		int year = keyboard.nextInt(); //get year value from keyboard
+
+		YearToCentury obj = new YearToCentury(year); //instantiation the YearToCentury object
+
+		System.out.println(year + " is " + obj.calCentury() + "th century." ); //print the calculated century value
+
+
 	}
 
 }
