3b1dce920609e36516faddcea7aca16650400a73
JoJeonghoon <akima95@naver.com>
2018-04-05 18:01:51 +0900

    Add single line comments

--- src/main/java/edu/handong/csee/java/lab07/prob3/Attendance.java
+++ src/main/java/edu/handong/csee/java/lab07/prob3/Attendance.java
@@ -1,4 +1,4 @@
-package edu.handong.csee.java.lab07.prob3;
+package edu.handong.csee.java.lab07.prob3; //package name
 
 
 /**
@@ -7,19 +7,19 @@ package edu.handong.csee.java.lab07.prob3;
  * @author jo jeong hoon
  *
  */
-public class Attendance {
+public class Attendance { //define the Attendance class
 
-	private String name;
-	private int grade;
-	private String studentNumber;
-	private int numOfAbsence;
+	private String name; //instance variable in private
+	private int grade; //instance variable in private
+	private String studentNumber; //instance variable in private
+	private int numOfAbsence; //instance variable in private
 	
 	
-	public Attendance() {//<constructor name = class name>
-		this.name = "Null";
-		this.grade =0;
-		this.studentNumber = "Null";
-		this.numOfAbsence = 0;
+	public Attendance() { //Constructor name is same with class name, and this is default constructor
+		this.name = "Null"; //set the instance variable name value as Null
+		this.grade =0; //set the instance variable grade value
+		this.studentNumber = "Null"; //set the instance variable studentNumber value
+		this.numOfAbsence = 0; //set the instance variable numOfAbsence value
 		
 	}
 	
@@ -31,44 +31,44 @@ public class Attendance {
 	 * @param studentNumber refers student's student number.
 	 * @param numOfAbsence refers the number of absence of student.
 	 */
-	public Attendance(String name, int grade, String studentNumber, int numOfAbsence) {
-	 this.name = name;
-	 this.grade = grade;
-	 this.studentNumber = studentNumber;
-	 this.numOfAbsence = numOfAbsence;
+	public Attendance(String name, int grade, String studentNumber, int numOfAbsence) { //constructor
+	 this.name = name; //set the instance variable name value as parameter value
+	 this.grade = grade; //set the instance variable grade value as parameter value  
+	 this.studentNumber = studentNumber; //set the instance variable studentNumber value as parameter value
+	 this.numOfAbsence = numOfAbsence; //set the instance variable numOfAbsence value as parameter value
 	 
 	}
-	public void setName(String name) {
-		this.name = name;
+	public void setName(String name) { //defines setName method
+		this.name = name; //set the instance variable name value as parameter value
 	}
 
-	public void setGrade(int grade) {
-		this.grade = grade;
+	public void setGrade(int grade) { //defines setGrade method
+		this.grade = grade; //set the instance variable grade value as parameter value
 	}
 	
-	public void setStudentNumber(String studentNumber) {
-		this.studentNumber = studentNumber;
+	public void setStudentNumber(String studentNumber) { //defines setStudentNumber method
+		this.studentNumber = studentNumber; //set the instance variable StudentNumber value as parameter value
 	}
 	
-	public void setNumOfAbsence(int numOfAbsence) {
-		this.numOfAbsence = numOfAbsence;
+	public void setNumOfAbsence(int numOfAbsence) { //defines setNumOfAbsence method
+		this.numOfAbsence = numOfAbsence; //set the instance variable NumOfAbsence value as parameter value
 		
 	}
 	
-	public String getName() {
-		return name;
+	public String getName() { //defines getName method
+		return name; //return name value
 	}
 	
-	public int getGrade() {
-		return grade;
+	public int getGrade() { //defines getGrade method
+		return grade; //return grade value
 	}
 	
-	public String getStudentNumber() {
-		return studentNumber;
+	public String getStudentNumber() { //defines getStudentNumber method
+		return studentNumber; //return studentNumber value
 	}
 	
-	public int getNumOfAbsence() {
-		return numOfAbsence;
+	public int getNumOfAbsence() { //defines getNumOfAbsence method
+		return numOfAbsence; //return numOfAbsence value
 		
 	}
 	
--- src/main/java/edu/handong/csee/java/lab07/prob3/Attendance_main.java
+++ src/main/java/edu/handong/csee/java/lab07/prob3/Attendance_main.java
@@ -1,6 +1,6 @@
-package edu.handong.csee.java.lab07.prob3;
+package edu.handong.csee.java.lab07.prob3; //package name
 
-import java.util.Random;
+import java.util.Random; //import Random class
 
 /**
  * This class is for detecting which student failed java programming course.
@@ -9,49 +9,49 @@ import java.util.Random;
  * @author jo jeong hoon
  *
  */
-public class Attendance_main {
+public class Attendance_main { //defines the Attendance_main class 
 
 	/**
 	 * This main method starts this program.
 	 * @param args refers command-line arguments as an array of String objects.
 	 */
-	public static void main(String[] args) {
+	public static void main(String[] args) { //defines main method
 		
-		Random randomGen = new Random();
+		Random randomGen = new Random(); //instantiate the Random object
 
 
-		Attendance Student1 = new Attendance("Jess", 4, "21400999" ,0);
-		Attendance Student2 = new Attendance("Kent", 2, "21700111", 0);
-		Attendance Student3 = new Attendance("Lucas", 1, "21833222", 0);
-		Attendance Student4 = new Attendance();
+		Attendance Student1 = new Attendance("Jess", 4, "21400999" ,0); //instantiate Attendance object and initialize with some values
+		Attendance Student2 = new Attendance("Kent", 2, "21700111", 0); //instantiate Attendance object and initialize with some values
+		Attendance Student3 = new Attendance("Lucas", 1, "21833222", 0); //instantiate Attendance object and initialize with some values
+		Attendance Student4 = new Attendance(); //instantiate Attendance object without initializing 
 
 		
-		Student4.setName("Martha");
-		Student4.setGrade(2);
+		Student4.setName("Martha"); //set the Student4 object's name 
+		Student4.setGrade(2); //set the Student4 grade 
 
-		Student1.setNumOfAbsence(randomGen.nextInt(10));
-		Student2.setNumOfAbsence(randomGen.nextInt(10));
-		Student3.setNumOfAbsence(randomGen.nextInt(10));
-		Student4.setNumOfAbsence(randomGen.nextInt(10));
+		Student1.setNumOfAbsence(randomGen.nextInt(10)); //set the Student1's number of absence randomly which is within 0 to 10
+		Student2.setNumOfAbsence(randomGen.nextInt(10)); //set the Student2's number of absence randomly which is within 0 to 10
+		Student3.setNumOfAbsence(randomGen.nextInt(10)); //set the Student3's number of absence randomly which is within 0 to 10
+		Student4.setNumOfAbsence(randomGen.nextInt(10)); //set the Student4's number of absence randomly which is within 0 to 10
 
-		if(Student1.getNumOfAbsence() > 6) {
-			System.out.println("I'm sorry, " + Student1.getName() + ". You failed this course.");
-			System.out.println("Your the number of absence is " + Student1.getNumOfAbsence()+ ".");
+		if(Student1.getNumOfAbsence() > 6) { //when the Student1's number of absence is greater than 6
+			System.out.println("I'm sorry, " + Student1.getName() + ". You failed this course."); //print this line
+			System.out.println("Your the number of absence is " + Student1.getNumOfAbsence()+ "."); //print this line
 		}
 		
-		if(Student2.getNumOfAbsence() > 6) {
-			System.out.println("I'm sorry, " + Student2.getName() + ". You failed this course.");
-			System.out.println("Your the number of absence is " + Student2.getNumOfAbsence()+ ".");
+		if(Student2.getNumOfAbsence() > 6) { //when the Student2's number of absence is greater than 6
+			System.out.println("I'm sorry, " + Student2.getName() + ". You failed this course."); //print this line
+			System.out.println("Your the number of absence is " + Student2.getNumOfAbsence()+ "."); //print this line
 		}
 		
-		if(Student3.getNumOfAbsence() > 6) {
-			System.out.println("I'm sorry, " + Student3.getName() + ". You failed this course.");
-			System.out.println("Your the number of absence is " + Student3.getNumOfAbsence()+ ".");
+		if(Student3.getNumOfAbsence() > 6) { //when the Student3's number of absence is greater than 6
+			System.out.println("I'm sorry, " + Student3.getName() + ". You failed this course."); //print this line
+			System.out.println("Your the number of absence is " + Student3.getNumOfAbsence()+ "."); //print this line
 		}
 	
-		if(Student4.getNumOfAbsence() > 6) {
-			System.out.println("I'm sorry, " + Student4.getName() + ". You failed this course.");
-			System.out.println("Your the number of absence is " + Student4.getNumOfAbsence()+ ".");
+		if(Student4.getNumOfAbsence() > 6) { //when the Student4's number of absence is greater than 6
+			System.out.println("I'm sorry, " + Student4.getName() + ". You failed this course."); //print this line
+			System.out.println("Your the number of absence is " + Student4.getNumOfAbsence()+ "."); //print this line
 		}
 		
 
